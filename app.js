7813175a0060416dcc6af4e17c499312a4bed89d
Emptiness002 <godgold11@gmail.com>
2023-04-05 01:24:48 +0000

    initial commit

--- app.js
+++ app.js
@@ -8,6 +8,30 @@ if (age <= 0) {
   alert("Age cannot be zero or negative. Please enter a valid age.");
 }
 
+function askQuestions(question) {
+  let answer = prompt(question + " (Yes/No):");
+  if (answer === "") {
+    answer = "invalid";
+  }
+  return answer;
+}
+
+function printAnswers(arr) {
+  console.log("Answers:");
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+let answers = [];
+
+answers.push(askQuestions("Do You Love the Quran?"));
+answers.push(askQuestions("Have you ever visited Japan?"));
+answers.push(askQuestions("Do you like to try new food?"));
+
+printAnswers(answers);
+
+
 let skipWelcomeMessage = confirm("Do you want to skip the welcoming message?");
 
 if (!skipWelcomeMessage) {
